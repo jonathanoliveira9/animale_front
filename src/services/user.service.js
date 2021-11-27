1b11efa5375a5f05c465be1816002f569a3cd6ac
jonathan <jonathanoliveirasilva9@gmail.com>
2021-11-27 01:29:08 -0200

    fix url

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,7 +3,7 @@ import authHeader from "./auth-header";
 
 class UserService {
   getPublicContent() {
-    return axios.get(process.env.REACT_APP_API_URL + "/api/v1/animals");
+    return axios.get(process.env.REACT_APP_API_URL + "api/v1/animals");
   }
 
   getUserBoard() {
